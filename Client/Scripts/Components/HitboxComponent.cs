1cd954c2ce204caf4e80783fa1c3d0deca2fd235
Danica Ellefsen Kvilhaug <65165280+Foxiecat@users.noreply.github.com>
2025-04-27 13:42:45 +0200

    Cleanup

--- Client/Scripts/Components/HitboxComponent.cs
+++ Client/Scripts/Components/HitboxComponent.cs
@@ -6,13 +6,5 @@ namespace NewGameProject.Scripts.Components;
 [GlobalClass]
 public partial class HitboxComponent : Area2D
 {
-	[Export] public HealthComponent HealthComponent;
-	
-	public HitboxComponent()
-	{
-		CollisionLayer = 2;
-		CollisionMask = 0;
-	}
-
 	
 }
