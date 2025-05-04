929ceab284932dbe85629d3a01f92876e1abf35f
mPareli <156788204+mPareli@users.noreply.github.com>
2025-05-04 21:13:43 +0200

    Update PlayerMoveComponent.cs

--- Client/Entities/Player/PlayerMoveComponent.cs
+++ Client/Entities/Player/PlayerMoveComponent.cs
@@ -1,10 +1,16 @@
 using Godot;
 using NewGameProject.Scripts.Components.Interfaces;
 
-namespace NewGameProject.Entities.Player;
+namespace NewGameProject.Scripts.Entities.Player.Components;
 
+/// <summary>
+/// Simple player moivement input provider
+/// Retruns directions based on input
+/// </summary>
 public partial class PlayerMoveComponent : Node, IMoveComponent
 {
+    [Export] public float MoveSpeed { get; set; } = 150f; // Player movement speed
+
+    // standard input actions for movement in games. Player moves in the direction pressed (WASD and Arrow keys)
     public Vector2 GetMovementDirection() => Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-    public float MoveSpeed { get; }
 }
